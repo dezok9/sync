de7e5cdfe1a721fb8d01ebe825683978b86b85fb
Destiny Ugonna Okonkwo <dezok@meta.com>
2024-06-26 16:03:23 -0700

    Basic login fields.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,16 @@ import Footer from '../components/Footer'
 function Login() {
     return (
         <>
-
+            <section className='login'>
+                <div className='login-section'>
+                    <h2>Username or Email</h2>
+                    <input className='input'></input>
+                </div>
+                <div className='login-section'>
+                    <h2>Password</h2>
+                    <input className='input'></input>
+                </div>
+            </section>
         </>
     )
 }
